c06db702a0a8981f463791ee0fb8fc7447efd014
hujintao56 <114620301+hujintao56@users.noreply.github.com>
2023-10-28 08:34:35 -0500

    fire

---
+++ Assets/Script/Fire.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    // 是否是火海，是=true
+    public bool isStrong;
+
+    // 是否在燃烧，燃烧=true
+    public bool isBurning;
+}
---
+++ Assets/Script/FireManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireManager : MonoBehaviour
+{
+    public List<Fire> strongFireList;
+
+    public void checkAllOut()
+    {
+        foreach (Fire fire in strongFireList)
+        {
+            if (fire.isBurning)
+            {
+                foreach (Fire f in strongFireList)
+                {
+                    f.isBurning = true;
+                }
+                return;
+            }
+            return;
+        }
+    }
+}
